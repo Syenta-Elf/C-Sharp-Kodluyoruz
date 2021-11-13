0934b643adea7fe8c82bfb4fa1e79618189ec9f3
Elif <elifnur.telase@gmail.com>
2021-11-13 17:14:00 +0200

    Karar yapilari olusturuldu.

---
+++ Karar Yapilari/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Karar_Yapilari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
