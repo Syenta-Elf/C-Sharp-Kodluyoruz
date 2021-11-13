5ab619db947dc2438d99346b074e42ece808bc59
Elif <elifnur.telase@gmail.com>
2021-11-13 02:17:46 +0200

    Operatorler eklendi

--- Operatorler/Program.cs
+++ Operatorler/Program.cs
@@ -6,7 +6,54 @@ namespace Operatorler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Atama ve İşlemli Atama Operatörleri (=, +=, -=, *=, /=)
+
+            int x=3;
+            x+=3;
+            x++;
+
+            //Mantıksal Operatörler (||, &&, !)
+            bool t=true;
+            bool f=false;
+            if(t&&f)
+            {
+                Console.WriteLine("&&");
+            }
+            if(t!&&f)
+            {
+                Console.WriteLine("!&&");
+            }
+            if(t||f)
+            {
+                Console.WriteLine("||");
+            }
+            //İlişkisel Operatörler (==,!=, <, >, >=,<=)
+            int a=3;
+            int b=5;
+
+            if(a==b)
+            {
+                Console.WriteLine("a==b");
+            }
+            if(a!=b)
+            {
+                Console.WriteLine("a!=b");
+            }
+            if(a>b)
+            {
+                Console.WriteLine("a>b");
+            }
+            if(b>a)
+            {
+                Console.WriteLine("b>a");
+            }
+
+            int k=3;
+            k=k+3; // k+=3;
+            k++;
+
+            // ?: koşul operatoru
+            // kalan=%
         }
     }
 }
