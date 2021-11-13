47d4ef5884e9efe803ff43c96e446090644b1ed9
Elif <elifnur.telase@gmail.com>
2021-11-13 20:13:35 +0200

    dizi kullanimi ornek eklendi

---
+++ Tanımlama, Erişim ve Döngülerle Dizi Kullanımı/Dizi Kullanimi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dizi_Kullanimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
