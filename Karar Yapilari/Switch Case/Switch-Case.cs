edb30ed922b6c059cf27fd5169fd048a1f9c11a6
Elif <elifnur.telase@gmail.com>
2021-11-13 18:10:25 +0200

    Switch-Case Ornekleri eklendi

---
+++ Karar Yapilari/Switch Case/Switch-Case.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Switch_Case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+            
+            //Expression
+           /* switch(month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Subat");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Mart");
+                    break;
+                case 4:
+                    Console.WriteLine("Nisan");
+                    break;
+                case 5:
+                    Console.WriteLine("Mayis");
+                    break;
+                case 6:
+                    Console.WriteLine("Haziran");
+                    break;
+
+
+                default:
+                    Console.WriteLine("Yanlis Veri.");
+                    break;
+                }*/
+                switch (month)
+                {
+                    case 12:
+                    case 1:
+                    case 2:
+                    Console.WriteLine("Kis Aylarindasiniz.");
+                    break;
+
+                    case 3:
+                    case 4:
+                    case 5:
+                    Console.WriteLine("Ilkbahar Aylarindasiniz");
+                    break;
+
+                    case 6:
+                    case 7:
+                    case 8:
+                    Console.WriteLine("Yaz Aylarindasiniz.");
+                    break;
+
+                    case 9:
+                    case 10:
+                    case 11:
+                    Console.WriteLine("Sonbahar Aylarindasiniz.");
+                    break;
+                    
+                    default:
+                    break;
+                }
+        }
+    }
+}
