ed5d48c85c1675455e109222c192d7f9f9627a2e
Elif <elifnur.telase@gmail.com>
2021-11-13 18:52:36 +0200

    While klasoru olusturuldu

---
+++ For Loop, Break, Continue/While Foreach/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace While_Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
