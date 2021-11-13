384e195c40b26d52e46d9491658acc43d0629cc9
Elif <elifnur.telase@gmail.com>
2021-11-13 20:14:15 +0200

    dizi kullanimi ornek eklendi

--- Tanımlama, Erişim ve Döngülerle Dizi Kullanımı/Dizi Kullanimi/Program.cs
+++ Tanımlama, Erişim ve Döngülerle Dizi Kullanımı/Dizi Kullanimi/Program.cs
@@ -6,7 +6,37 @@ namespace Dizi_Kullanimi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] renkler = {"mor","kirmizi","sari","pembe","mavi"};
+            string[] hayvanlar = new string[5];
+
+            hayvanlar[3]="kopke";
+            int[] dizi;
+            dizi = new int[5];
+            dizi[3]=10;
+
+            Console.WriteLine("***renkler***");
+            foreach (var renk in renkler)
+            {
+                Console.WriteLine(renk);
+            }
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(hayvanlar[3]);
+            //Dongulerle dizi kullanimi
+
+            Console.WriteLine("Lutfen dizinin eleman sayisini girin");
+            int elemanS= int.Parse(Console.ReadLine());
+            int[] dizi2 = new int[elemanS];
+            float toplam=0f;
+            
+            for(int i=0;i<elemanS;i++)
+            {   
+                Console.WriteLine("Sayi girin:");
+                dizi2[i]=int.Parse(Console.ReadLine());
+                toplam+=dizi2[i];
+            }
+
+            Console.WriteLine("Dizinin Ortalamasi:"+toplam/elemanS); 
+            
         }
     }
 }
