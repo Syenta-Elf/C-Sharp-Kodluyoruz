e2fcbd9b36a77108ef332f25a1b40f97547cc333
Elif <elifnur.telase@gmail.com>
2021-11-13 18:50:51 +0200

    For dongu Ornekleri eklendi.

--- For Loop, Break, Continue/For Loop/Program.cs
+++ For Loop, Break, Continue/For Loop/Program.cs
@@ -5,8 +5,34 @@ namespace For_Loop
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {   
+            Console.WriteLine("Bir sayi giriniz:");
+            int sayac = int.Parse(Console.ReadLine());
+
+            for(int i=0;i<=sayac;i++)
+            {
+                if(i%2==1) Console.WriteLine(i);
+            }
+
+            int ciftToplam=0 , tekToplam=0;
+
+            for(int i=0;i<=1000;i++)
+            {
+                if(i%2==1) tekToplam+=+i;
+                else ciftToplam+=+i; 
+            }
+            Console.WriteLine("Tek Toplam:"+tekToplam+"\nCift Toplam:"+ciftToplam);
+
+            for(int i=0;i<10;i++)
+            {   
+                Console.WriteLine(i);
+                if(i==4) break; //donguden cikti
+            }
+            for(int i=0;i<10;i++)
+            {   
+                Console.WriteLine(i);
+                if(i==4) continue;//dongu devam etti
+            }
         }
     }
 }
