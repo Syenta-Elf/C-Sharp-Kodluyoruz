377e1f830f5ce2387c5002f134eb8ecd0a0382a3
Elif <elifnur.telase@gmail.com>
2021-11-13 19:15:36 +0200

    While ornekleri eklendi

--- For Loop, Break, Continue/While Foreach/Program.cs
+++ For Loop, Break, Continue/While Foreach/Program.cs
@@ -5,8 +5,31 @@ namespace While_Foreach
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {   
+            Console.WriteLine("Bir sayi giriniz:");
+            int sayac = int.Parse(Console.ReadLine());
+            int i = 1,toplam=0;
+            while(sayac>=i)
+            {
+                toplam+=i;
+                i++;
+            }
+            
+            Console.WriteLine("Ortalama:"+toplam/sayac);
+            char character ='a';
+            while (character<='z')
+            {
+                 Console.Write(character);
+                 character++;
+            }
+
+            Console.WriteLine("\n***Foreach***");
+
+            string[] arabalar = {"bmw","ford","ferrari","toyota"};
+            foreach (var item in arabalar)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
