2126409f1310a2228e6eb6b3a7bf4c68aff2c0e7
Elif <elifnur.telase@gmail.com>
2021-11-13 18:28:22 +0200

    Loop klasoru olusturuldu

---
+++ For Loop, Break, Continue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace For_Loop__Break__Continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
