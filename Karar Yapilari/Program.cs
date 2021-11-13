cfcd5a5904a8596005abb761626379b44e9545d6
Elif <elifnur.telase@gmail.com>
2021-11-13 17:27:21 +0200

    If ElseIf Ternary Ornekleri eklendi.

--- Karar Yapilari/Program.cs
+++ Karar Yapilari/Program.cs
@@ -6,7 +6,26 @@ namespace Karar_Yapilari
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int time = DateTime.Now.Hour;
+            /*
+            if( time<11 && time>=6)
+            {
+                Console.WriteLine("Iyi Sabahlar");
+            }
+
+            else if(time<=18)
+            {
+                Console.WriteLine("Iyi Gunler!");
+            }
+            else
+            {
+                Console.WriteLine("Iyi Aksamlar!");
+            }
+            */
+            
+            //string sonuc = time<=18 ? "Iyi gunler" : "Iyi Aksamlar"; 
+            string sonuc = time >=6 && time<=11 ? "Gunaydin!" : time<=18 ? "Iyi Gunler!" : "Iyi Aksamlar!";
+            Console.WriteLine(sonuc);
         }
     }
 }
