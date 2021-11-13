942e3bbcfe62003cd39c4d5156e705c64a4bf1d3
Elif <elifnur.telase@gmail.com>
2021-11-13 13:59:43 +0200

    Try Catch Finally Ve Mantıksal Hatalar olusturuldu

---
+++ Try Catch Finally Ve Mantiksal Hatalar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Try_Catch_Finally_Ve_Mantiksal_Hatalar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
