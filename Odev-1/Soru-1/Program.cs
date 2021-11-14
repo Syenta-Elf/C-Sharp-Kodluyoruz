a81128e2204f92d471dbe40b3d0c9a584bb1d19c
Elif <elifnur.telase@gmail.com>
2021-11-14 15:29:39 +0200

    Ilk Soru bitti,dogru calisiyor.

--- Odev-1/Soru-1/Program.cs
+++ Odev-1/Soru-1/Program.cs
@@ -6,7 +6,36 @@ namespace Soru_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        /*
+        * Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n).
+        * Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin.
+        * Kullanıcının girmiş olduğu sayılardan çift olanlar console'a yazdırın.
+        */
+        System.Console.WriteLine("Bir sayi girin:");
+        
+        int sayi = Convert.ToInt32(Console.ReadLine());
+
+        System.Console.WriteLine(sayi+" tane sayi girin:");
+        int[] sayilar = new int[sayi];
+        for (int i = 0; i < sayi; i++)
+        {
+            Console.Write("Sayi gir:");
+            sayilar[i]=Convert.ToInt32(Console.ReadLine());
+        }
+        sayilar.ciftGoster();
+
+        }
+
+    }
+    public static class Extension
+    {
+        public static void ciftGoster(this int[] param)
+        {
+            System.Console.WriteLine("Girdiginiz cift sayilar:");
+            foreach (var item in param)
+            {
+                if(item%2==0) Console.Write(item+" ");
+            }
         }
     }
 }
