0e2dd3789d22039112a13a253d7ae7da6c53ae92
Elif <elifnur.telase@gmail.com>
2021-11-14 12:38:06 +0200

    Metot Tanimlama Olusturuldu

---
+++ Metotlar/Metot Tanımlama/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Metot_Tanımlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
