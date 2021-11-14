b50b9c0bb33babc69fde2154d1486487798af556
Elif <elifnur.telase@gmail.com>
2021-11-14 15:58:26 +0200

    Ucuncu soru olusturuldu

---
+++ Odev-1/Soru-3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Soru_3
+{
+    class Program
+    {
+        // Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin (n).
+        // Sonrasında kullanıcıdan n adet kelime girmesi isteyin.
+        // Kullanıcının girişini yaptığı kelimeleri sondan başa doğru console'a yazdırın.
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
