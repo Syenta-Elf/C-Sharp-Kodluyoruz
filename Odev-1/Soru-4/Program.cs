9ed37c64b64a7d27e9e0012a817901a44584f758
Elif <elifnur.telase@gmail.com>
2021-11-14 16:28:34 +0200

    Dorduncu soru dogru calisiyor.

--- Odev-1/Soru-4/Program.cs
+++ Odev-1/Soru-4/Program.cs
@@ -4,9 +4,35 @@ namespace Soru_4
 {
     class Program
     {
+        // Bir konsol uygulamasında kullanıcıdan bir cümle yazması isteyin.
+        // Cümledeki toplam kelime ve harf sayısını console'a yazdırın.
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Bir cümle girin");
+            string cumle = Console.ReadLine();
+            kacKelimeVar(cumle);
+            kacHarfVar(cumle);
+        }
+
+        static void kacKelimeVar(string cumle)
+        {
+            string[] kelimeler = cumle.Split(" ");
+            Console.WriteLine("Kelime Sayisi: "+kelimeler.Length);
+        }
+        
+        static void kacHarfVar(string cumle)
+        {
+            string liste = "ABCÇDEFGĞHİIJKLMNOÖPRSŞTUÜVYZ"
+                      + "abcçdefgğhiıjklmnoöprsştuüvyz";
+            int sayac = 0;
+
+            for (int i = 0; i < cumle.Length; i++)
+            {
+                if (liste.Contains(cumle[i]))
+                    sayac++;
+            }
+
+            Console.WriteLine("toplam "+sayac+" tane harf vardır.");
         }
     }
 }
