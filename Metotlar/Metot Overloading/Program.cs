e46f79110eabba61eb11127fbf1a181b94405769
Elif <elifnur.telase@gmail.com>
2021-11-14 13:50:10 +0200

    Metot Overloading ornekler eklendi.

--- Metotlar/Metot Overloading/Program.cs
+++ Metotlar/Metot Overloading/Program.cs
@@ -6,7 +6,44 @@ namespace Metot_Overloading
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string a="999";
+            
+            bool sonuc = int.TryParse(a, out int sayi);
+
+            if(sonuc) 
+            {
+                Console.WriteLine("Basarili.");
+                Console.WriteLine(sayi);
+            }
+            else Console.WriteLine("Basarisiz");
+            Metotlar deneme = new Metotlar();
+            deneme.Topla(3,4,out int toplamSonuc);
+            Console.WriteLine(toplamSonuc);
+            deneme.EkranaYazdir("Merhaba");
+            deneme.EkranaYazdir("Merhaba Dunya");
+            deneme.EkranaYazdir(1);
         }
+
+    }
+    class Metotlar
+    {
+        public void Topla(int a,int b,out int topla)
+        {
+            topla=a+b;
+        }
+
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(string veri1,string veri2)
+        {
+            Console.WriteLine(veri1+veri2);
+        }
+        public void EkranaYazdir(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+
     }
 }
