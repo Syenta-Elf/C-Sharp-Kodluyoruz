e84ee8bba639b279356fc2872d43e8e6a82e3213
Elif <elifnur.telase@gmail.com>
2021-11-14 13:25:06 +0200

    Metot Overloading Olusturuldu.

---
+++ Metotlar/Metot Overloading/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Metot_Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
