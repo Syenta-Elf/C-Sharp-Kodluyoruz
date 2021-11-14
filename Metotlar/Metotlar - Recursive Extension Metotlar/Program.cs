e3ce75b92f857c0a08d07f50a3e654243b536b22
Elif <elifnur.telase@gmail.com>
2021-11-14 14:01:42 +0200

    Metot Recursive Extension olusturuldu.

---
+++ Metotlar/Metotlar - Recursive Extension Metotlar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Metotlar___Recursive_Extension_Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
