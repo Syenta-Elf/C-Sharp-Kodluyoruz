2b55bdd1bc9dde68938bf0f047991b7a082a23b8
Elif <elifnur.telase@gmail.com>
2021-11-14 13:20:31 +0200

    Metotlar Tanimlama Ornekleri eklendi.

--- Metotlar/Metot Tanımlama/Program.cs
+++ Metotlar/Metot Tanımlama/Program.cs
@@ -6,7 +6,30 @@ namespace Metot_Tanımlama
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a=3,b=4;
+            int sonuc =Topla(a,b);
+            Console.WriteLine("Sonuc:"+sonuc);
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYazdir("Sonuc: "+sonuc.ToString());
+            ornek.EkranaYazdir("Arttir ve topla:"+ornek.ArttirVeTopla(ref a,ref b).ToString());
+            Console.WriteLine("a:"+a+" b:"+b);
+        }
+        static int Topla(int deger1,int deger2)
+        {
+            return(deger1+deger2);
+        }
+    }
+    class Metotlar
+    {
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public int ArttirVeTopla(ref int deger1,ref int deger2)
+        {   
+            deger1++;deger2++;
+            return(deger1+deger2);
         }
     }
 }
