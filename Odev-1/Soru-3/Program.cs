8bb6e3e0dd34ce67722f105d7c0091eb84dff0a5
Elif <elifnur.telase@gmail.com>
2021-11-14 16:10:54 +0200

    Ucuncu soru dogru calisiyor.

--- Odev-1/Soru-3/Program.cs
+++ Odev-1/Soru-3/Program.cs
@@ -9,7 +9,41 @@ namespace Soru_3
         // Kullanıcının girişini yaptığı kelimeleri sondan başa doğru console'a yazdırın.
         static void Main(string[] args)
         {
+           System.Console.WriteLine("Bir sayi girin:");
+           int n=Convert.ToInt32(Console.ReadLine());
+           string[] kelimeler = kelime(n);
            
+           goster(kelimeler);
+           reverse(kelimeler);
+           goster(kelimeler);
+        }
+
+        static string[] kelime (int n)
+        {
+            string[] kelimeler = new string[n];
+            System.Console.WriteLine(n+" tane kelime girin:");
+            
+            for (int i = 0; i < n; i++)
+            {
+                System.Console.Write("Kelime girin:");
+                kelimeler[i]=Console.ReadLine();
+            }
+            return kelimeler;
+        }
+
+        static string[] reverse(string[] kelimeler)
+        {
+            Array.Reverse(kelimeler);
+            return kelimeler;
+        }
+
+        static void goster(string[] kelimeler)
+        {
+            foreach (var kelime in kelimeler)
+            {
+                System.Console.Write(kelime+" ");
+            }
+            System.Console.WriteLine("");//alta satira gecmesi icin.
         }
     }
 }
