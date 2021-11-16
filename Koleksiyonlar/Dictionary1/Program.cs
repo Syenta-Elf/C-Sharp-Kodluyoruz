93c08a81537429fa72685f51d1cc79f30b2e7689
Elif <elifnur.telase@gmail.com>
2021-11-16 13:58:25 +0200

    Dictionary1 Ornekler eklendi.

--- Koleksiyonlar/Dictionary1/Program.cs
+++ Koleksiyonlar/Dictionary1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Dictionary1
 {
@@ -6,7 +7,56 @@ namespace Dictionary1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //system.collections.generic;
+            Dictionary<int,string> kullanicilar = new Dictionary<int, string>();
+            kullanicilar.Add(1,"Kawai");
+            kullanicilar.Add(2,"Ayse");
+            kullanicilar.Add(18,"Deniz");
+            kullanicilar.Add(21,"Ozcan");
+            
+            //dizi elemanlarina erisim
+            System.Console.WriteLine("*****Erisim*****");
+            System.Console.WriteLine(kullanicilar[2]);
+
+            foreach (var item in kullanicilar)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            //Count
+            System.Console.WriteLine("****Count****");
+            System.Console.WriteLine(kullanicilar.Count);
+
+            //Contains
+            System.Console.WriteLine("****Contains****");
+            System.Console.WriteLine(kullanicilar.ContainsKey(18));
+            System.Console.WriteLine(kullanicilar.ContainsValue("Kawai"));
+
+            //Remove
+            System.Console.WriteLine("****Remove****");
+            kullanicilar.Remove(21);
+            foreach (var item in kullanicilar)
+            {
+                System.Console.WriteLine(item.Key);
+            }
+
+            //Keys
+            System.Console.WriteLine("****Keys****");
+            foreach (var item in kullanicilar.Keys)
+            {
+                System.Console.WriteLine(item);
+            }
+            
+            //Values
+            System.Console.WriteLine("****Values****");
+            foreach (var item in kullanicilar.Values)
+            {
+                System.Console.WriteLine(item);
+            }
+
+
+
+
         }
     }
 }
