d8745a6d260c8f3b909bba215d6df4820398e750
Elif <elifnur.telase@gmail.com>
2021-11-16 13:16:01 +0200

    ArrayList Ornekler eklendi.

--- Koleksiyonlar/ArrayList1/Program.cs
+++ Koleksiyonlar/ArrayList1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace ArrayList1
 {
@@ -8,7 +9,62 @@ namespace ArrayList1
         static void Main(string[] args)
         {
             ArrayList list = new ArrayList();
+            // list.Add("Ayse");
+            // list.Add(2);
+            // list.Add(true);
+            //list.Add('A');
+
+            //icerisindeki verilere erisim
+
+            //System.Console.WriteLine(list[0]);
+
+            foreach (var item in list)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            //AddRange birden fazla elemani ekleme
+            System.Console.WriteLine("*********AddRange**********");
+            //string[] renkler = {"mor","kirmizi","mavi","sari","pembe"};
+            List<int> sayilar = new List<int>() {1,2,5,4,3};
             
+            //list.AddRange(renkler);
+            list.AddRange(sayilar);
+
+            foreach (var item in list)
+            {
+                System.Console.WriteLine(item.ToString());
+            }
+
+            //Sort
+            System.Console.WriteLine("******Sort*****");
+            list.Sort();
+            
+            foreach (var item in list)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            //BinarySearch
+            System.Console.WriteLine("*******Binary*******");
+            System.Console.WriteLine(list.BinarySearch(3));
+
+            //Reverse
+            System.Console.WriteLine("*******Reverse*******");
+            list.Reverse();
+            foreach (var item in list)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            //Clear
+            list.Clear(); //listeyi temizle
+            System.Console.WriteLine("*******Clear*******");
+            foreach (var item in list)
+            {
+                System.Console.WriteLine(item);
+            }
+
         }
     }
 }
