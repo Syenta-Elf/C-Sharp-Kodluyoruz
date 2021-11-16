1365ea369de436d2684cede18d99016bcb0775d9
Elif <elifnur.telase@gmail.com>
2021-11-16 11:48:00 +0200

    Generic Koleksiyonlar ve List Olusturuldu.

---
+++ Koleksiyonlar/Generic Koleksiyonlar ve List/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generic_Koleksiyonlar_ve_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
