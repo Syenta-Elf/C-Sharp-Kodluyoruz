258bb9aea9c6f29ab76e98b9affe944bbb7abbcd
Elif <elifnur.telase@gmail.com>
2021-11-17 16:28:24 +0200

    Static Sinif ve Uyeler olusturuldu.

---
+++ Class Kavrami/Static Sinif ve Uyeler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Static_Sinif_ve_Uyeler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
