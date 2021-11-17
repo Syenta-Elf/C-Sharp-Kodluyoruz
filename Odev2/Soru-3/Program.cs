258a02f1752e1672cc7aa1aeba6791cb92a9bd72
Elif <elifnur.telase@gmail.com>
2021-11-17 13:31:38 +0200

    Soru-3 olusturuldu.

---
+++ Odev2/Soru-3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Soru_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
