33f3d471c0cca896a75b3d19222e68b12c7344a6
Elif <elifnur.telase@gmail.com>
2021-11-21 11:54:11 +0200

    Struct kavrami ornekleri eklendi.

--- Class Kavrami/Struct Kavrami/Program.cs
+++ Class Kavrami/Struct Kavrami/Program.cs
@@ -6,7 +6,40 @@ namespace Struct_Kavrami
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dikdortgen dikdortgen = new Dikdortgen();
+            System.Console.WriteLine("class alan hesabi:"+dikdortgen.alanHesapla());
+
+            Dikdortgen_Struct dikdortgen_Struct = new Dikdortgen_Struct(3,4);
+            //Dikdortgen_Struct dikdortgen_Struct = new Dikdortgen_Struct();
+            System.Console.WriteLine("struct alan hesabi:"+dikdortgen_Struct.alanHesapla());
+        }
+    }
+    class Dikdortgen
+    {
+        public int kisaKenar;
+        public int uzunKenar;
+        public Dikdortgen(){
+            kisaKenar=3;
+            uzunKenar=4;
+        }
+
+        public long alanHesapla()
+        {
+            return this.kisaKenar*this.uzunKenar;
+        }
+    }
+    struct Dikdortgen_Struct
+    {
+        public int kisaKenar;
+        public int uzunKenar;
+        public Dikdortgen_Struct(int kisaKenar, int uzunKenar)
+        {
+            this.kisaKenar = kisaKenar;
+            this.uzunKenar = uzunKenar;
+        }
+        public long alanHesapla()
+        {
+            return this.kisaKenar*this.uzunKenar;
         }
     }
 }
