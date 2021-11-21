e2cf8534f94eea737b8c3704e44e7bc78394abc4
Elif <elifnur.telase@gmail.com>
2021-11-21 12:15:05 +0200

    Enum ornekleri eklendi.

--- Class Kavrami/Enum Nedir/Program.cs
+++ Class Kavrami/Enum Nedir/Program.cs
@@ -6,7 +6,37 @@ namespace Enum_Nedir
     {
         static void Main(string[] args)
         {
-            
+            System.Console.WriteLine(Gunler.Pazar);    
+            System.Console.WriteLine((int)Gunler.Cumartesi);
+            int sicak = 35;
+            if(sicak<=(int)HavaDurumu.Normal)
+            {
+                System.Console.WriteLine("Disariya cikmak icin havanin isinmasini bekleyelim");
+            }
+            else if (sicak>=(int)HavaDurumu.Sicak)
+            {
+                System.Console.WriteLine("Disariya cikmak icin cok sicak");
+            }
+            else if (sicak>=(int)HavaDurumu.Normal&& sicak<(int)HavaDurumu.CokSicak)
+            {
+                System.Console.WriteLine("Disari cikalim");
+            }
         }
+    }
+    enum Gunler{
+        Pazartesi=1,
+        Sali,
+        Carsamba,
+        Persembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+    enum HavaDurumu
+    {
+        Soguk=5,
+        Normal=20,
+        Sicak=25,
+        CokSicak=30
     }
 }
