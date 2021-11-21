6f22bdaccd705b75770cc33a00d56ec81d2ff869
Elif <elifnur.telase@gmail.com>
2021-11-21 11:37:04 +0200

    Struct kavrami olusturuldu.

---
+++ Class Kavrami/Struct Kavrami/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Struct_Kavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
