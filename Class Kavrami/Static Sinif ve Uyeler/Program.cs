2fd6ccded301de5bb2d4aac29a35c02b57d85cb4
Elif <elifnur.telase@gmail.com>
2021-11-21 11:32:37 +0200

    Static Sinif ornekleri eklendi.

--- Class Kavrami/Static Sinif ve Uyeler/Program.cs
+++ Class Kavrami/Static Sinif ve Uyeler/Program.cs
@@ -6,7 +6,43 @@ namespace Static_Sinif_ve_Uyeler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Calisan Sayisi:"+Calisan.CalisanSayisi);
+
+            Calisan calisan = new Calisan("Ayse","Yilmaz","IK");
+            System.Console.WriteLine("Calisan Sayisi:"+Calisan.CalisanSayisi);
+            Islemler.Topla(200,300);
+            Islemler.Cikar(100,50);
+        }
+    }
+    class Calisan
+    {
+        private static int calisanSayisi;
+        public static int CalisanSayisi { get => calisanSayisi;}
+
+        private string isim;
+        private string soyIsim;
+        private string departman;
+        static Calisan(){
+            calisanSayisi=0;
+
+        }
+        public Calisan(string isim, string soyIsim, string departman)
+        {
+            this.isim = isim;
+            this.soyIsim = soyIsim;
+            this.departman = departman;
+            calisanSayisi++;
+        }
+    }
+    static class Islemler
+    {
+        public static long Topla(int sayi1,int sayi2)
+        {
+            return sayi1+sayi2;
+        }
+        public static long Cikar(int sayi1,int sayi2)
+        {
+            return sayi1-sayi2;
         }
     }
 }
